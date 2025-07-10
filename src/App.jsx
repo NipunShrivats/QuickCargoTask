1f2c2d309ad9810ab524003d94c47461f58810de
NipunShrivats <nipunrawat16@gmail.com>
2025-07-10 22:13:46 +0530

    changes on Jul10, change no.06

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-// import UserModal from "./components/UserModal";
 import Test1 from "./components/Test1";
 export default function App() {
   return (
     <>
-      {/* <UserModal /> */}
       <Test1 />
     </>
   );
